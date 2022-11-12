a74bdd68041a5cbbc2e8d59f338a438f30f48f99
Jorge Garcia <jgarciam@gmail.com>
2022-11-12 15:30:23 -0300

    first commit

---
+++ servicio2/src/main/java/cl/trabajando/servicio2/Servicio2Application.java
@@ -0,0 +1,20 @@
+package cl.trabajando.servicio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Servicio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Servicio2Application.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	   return builder.build();
+	}
+}
